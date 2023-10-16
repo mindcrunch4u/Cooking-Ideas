29c884e467050ba2dcc613461c652f6f0cdf03c2
zywang <zywang@sonicwall.com>
2023-10-16 11:10:37 +0800

    localization, support English and Chinese

--- app/src/main/java/com/bob/info/ui/cook/CookFragment.java
+++ app/src/main/java/com/bob/info/ui/cook/CookFragment.java
@@ -43,7 +43,7 @@ public class CookFragment extends Fragment implements View.OnClickListener {
         text_view.setMovementMethod(new ScrollingMovementMethod());
 
         cook_text = root.findViewById(R.id.text_cook);
-        cook_text.setText("今天煮什么");
+        cook_text.setText(getString(R.string.fragment_cook_title));
 
         return root;
     }
@@ -66,11 +66,11 @@ public class CookFragment extends Fragment implements View.OnClickListener {
         if (v.getId() == R.id.button_copy){
             String currentText = cook_text.getText().toString();
             setClipboard(getContext(), currentText);
-            Toast.makeText(getContext(), "copied", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), getString(R.string.util_copied_text), Toast.LENGTH_SHORT).show();
         }
         else if (v.getId() == R.id.button_renew){
             String bigString = "" + System.lineSeparator();
-            ArrayList<ArrayList<String>> basket = new ReceiptGenerator().generate();
+            ArrayList<ArrayList<String>> basket = new ReceiptGenerator(getContext()).generate();
             for( ArrayList list : basket ){
                 if (list.get(0) == "none" || list.get(1) == ""){
                     continue;
--- app/src/main/java/com/bob/info/ui/cook/ReceiptGenerator.java
+++ app/src/main/java/com/bob/info/ui/cook/ReceiptGenerator.java
@@ -1,4 +1,7 @@
 package com.bob.info.ui.cook;
+import android.content.Context;
+import android.content.res.Resources;
+import com.bob.info.R;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -9,6 +12,7 @@ import java.util.Random;
 
 
 public class ReceiptGenerator {
+    private Context context = null;
     private static class Types {
         public static int none =0;
         public static int vegetable =1;
@@ -18,74 +22,6 @@ public class ReceiptGenerator {
         public static int shape =5;
         public static int meat =6;
         public static int carb =7;
-
-        public static String intToName(int type){
-            String name = "";
-            switch (type) {
-                case 0:
-                    name = "none";
-                    break;
-                case 1:
-                    name = "vegetable";
-                    break;
-                case 2:
-                    name = "misc";
-                    break;
-                case 3:
-                    name = "seasoning";
-                    break;
-                case 4:
-                    name = "method";
-                    break;
-                case 5:
-                    name = "shape";
-                    break;
-                case 6:
-                    name = "meat";
-                    break;
-                case 7:
-                    name = "carb";
-                    break;
-                default:
-                    name = "unknown";
-                    break;
-            }
-            return name;
-        }
-
-        public static String intToNameCn(int type){
-            String name = "";
-            switch (type) {
-                case 0:
-                    name = "none";
-                    break;
-                case 1:
-                    name = "蔬菜";
-                    break;
-                case 2:
-                    name = "其他";
-                    break;
-                case 3:
-                    name = "调味";
-                    break;
-                case 4:
-                    name = "做法";
-                    break;
-                case 5:
-                    name = "形状";
-                    break;
-                case 6:
-                    name = "肉类";
-                    break;
-                case 7:
-                    name = "碳水";
-                    break;
-                default:
-                    name = "unknown";
-                    break;
-            }
-            return name;
-        }
     }
     private class MinMax{
         public int min = 0;
@@ -98,14 +34,45 @@ public class ReceiptGenerator {
 
     private Dictionary<Integer, String> ingredients = null;
     private Dictionary<Integer, MinMax> required_amount  = null;
-
+    public String intToTranslatedName(int type){
+        String name = "";
+        switch (type) {
+            case 0:
+                name = context.getResources().getString(R.string.fragment_cook_type_none);
+                break;
+            case 1:
+                name = context.getResources().getString(R.string.fragment_cook_type_vegetable);
+                break;
+            case 2:
+                name = context.getResources().getString(R.string.fragment_cook_type_misc);
+                break;
+            case 3:
+                name = context.getResources().getString(R.string.fragment_cook_type_seasoning);
+                break;
+            case 4:
+                name = context.getResources().getString(R.string.fragment_cook_type_method);
+                break;
+            case 5:
+                name = context.getResources().getString(R.string.fragment_cook_type_shape);
+                break;
+            case 6:
+                name = context.getResources().getString(R.string.fragment_cook_type_meat);
+                break;
+            case 7:
+                name = context.getResources().getString(R.string.fragment_cook_type_carb);
+                break;
+            default:
+                name = context.getResources().getString(R.string.fragment_cook_type_unknown);
+                break;
+        }
+        return name;
+    }
     public int getRandomNumberUsingNextInt(int min, int max) {
         Random random = new Random();
         int ret =random.nextInt(max - min + 1) + min;
         return ret;
     }
     private int[] randomIntList(int minLength, int maxLength, int minValue, int maxValue){
-        Random rand = new Random();
         int arraySize = getRandomNumberUsingNextInt(minLength, maxLength);
         int[] ret = new int [arraySize];
         for(int index=0; index<arraySize; index++){
@@ -114,18 +81,19 @@ public class ReceiptGenerator {
         return ret;
     }
 
-    public ReceiptGenerator(){
+    public ReceiptGenerator(Context context){
+        this.context = context;
         this.ingredients= new Hashtable<>();
         this.required_amount = new Hashtable<>();
 
         ingredients.put(Types.none, "");
-        ingredients.put(Types.vegetable, "茄子，豆角，生菜，菜心，甜椒，土豆，白菜，娃娃菜，菠菜，菌菇，黄瓜，丝瓜，南瓜，苦瓜，包菜，番茄，苋菜，韭菜，豆芽，荷兰豆，木耳，红萝卜，上海青，蒜苔，芹菜，菜心");
-        ingredients.put(Types.misc, "豆皮，豆干，虾皮，鸡蛋，皮蛋，豆腐" );
-        ingredients.put(Types.seasoning     , "洋葱，玉米，大葱，小葱，葱叶" );
-        ingredients.put(Types.method        , "黑椒，大料，糖盐炒，蒜香，蚝酱油，凉拌，水煮，蒸，豆瓣酱，咖喱" );
-        ingredients.put(Types.shape         , "条，丝，粒，片，块，沫，球/饼" );
-        ingredients.put(Types.meat          , "鸡蛋，五花肉，猪肉，排骨，猪肝，鸡翅，鸡腿，鸡腿肉，鸡胸，牛排，牛腱，牛腩，肥牛片，虾，扇贝" );
-        ingredients.put(Types.carb          , "米饭，面饼，意面，小米粥，粥" );
+        ingredients.put(Types.vegetable, this.context.getResources().getString(R.string.fragment_cook_array_vegetable));
+        ingredients.put(Types.misc, this.context.getResources().getString(R.string.fragment_cook_array_misc));
+        ingredients.put(Types.seasoning     , this.context.getResources().getString(R.string.fragment_cook_array_seasoning));
+        ingredients.put(Types.method        , this.context.getResources().getString(R.string.fragment_cook_array_method));
+        ingredients.put(Types.shape         , this.context.getResources().getString(R.string.fragment_cook_array_shape));
+        ingredients.put(Types.meat          , this.context.getResources().getString(R.string.fragment_cook_array_meat));
+        ingredients.put(Types.carb          , this.context.getResources().getString(R.string.fragment_cook_array_carb));
 
         required_amount.put(Types.none          , new MinMax(0,0));
         required_amount.put(Types.vegetable     , new MinMax(1,3));
@@ -143,18 +111,19 @@ public class ReceiptGenerator {
         Enumeration<Integer> all_ingredients = ingredients.keys();
         while(all_ingredients.hasMoreElements()){
             int key = all_ingredients.nextElement();
-            String name = Types.intToNameCn(key);
+            String name = intToTranslatedName(key);
             ArrayList<String> current = new ArrayList<>();
             current.add(name);
-            String[] items = new String(ingredients.get(key)).split("，", 0);
+            String[] items = new String(ingredients.get(key)).split(",", 0);
             int required_min = required_amount.get(key).min;
             int required_max = required_amount.get(key).max;
             int[] pickedItems = randomIntList(required_min, required_max, 0, items.length-1);
             ArrayList<String> currentSelections = new ArrayList<>();
             for(int index : pickedItems){
-                currentSelections.add(items[index]);
+                currentSelections.add(items[index].trim());
             }
-            current.add(String.join(", ", currentSelections));
+            String comma = this.context.getResources().getString(R.string.text_comma);
+            current.add(String.join(comma, currentSelections));
             ret.add(current);
         }
         return ret;
